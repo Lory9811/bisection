e8b9b6961a81751f0716799c9b0e938d66aa9a76
Lorenzo Manna <Lory9811@users.noreply.github.com>
2017-03-08 21:55:51 +0100

    Changed version from 1.0 to 0.1

--- python/src/setup.py
+++ python/src/setup.py
@@ -4,7 +4,7 @@ from cx_Freeze import setup, Executable
 additional_modules = ['numpy.core._methods', 'numpy.lib.format']
 setup(
     name = "Bisection Algorithm",
-    version = "1.0",
+    version = "0.1",
     options = {'build_exe': {'includes': additional_modules}},
     executables = [Executable(script="bisection.py")]
 )
